fff695b7a7d82227bb7ceccd1eb3d4306e0e75b8
Anna Serova <serova@hpe.com>
2020-04-17 21:08:21 +0300

    uncommented implicitly_wait

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,11 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from .locators import BasePageLocators
 
 class BasePage():
-	#def __init__(self, browser, url, timeout=10):
-	def __init__(self, browser, url):
+	def __init__(self, browser, url, timeout=10):
 		self.browser = browser
 		self.url = url
-		#self.browser.implicitly_wait(timeout)
+		self.browser.implicitly_wait(timeout)
 	def open(self):
 		self.browser.get(self.url)
 	def openBasket(self, how, what):
