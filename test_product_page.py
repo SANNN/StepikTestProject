63f51997f97f79aeba0c3b3b420801fe5b2c7c0e
Anna Serova <serova@hpe.com>
2020-04-17 21:31:10 +0300

    removed redundand links

--- test_product_page.py
+++ test_product_page.py
@@ -5,6 +5,8 @@ from .pages.login_page import LoginPage
 import time
 import pytest
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
 #@pytest.mark.skip(reason="module 4.3 step 4 -- expected pass")
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", \
@@ -32,21 +34,18 @@ def test_guest_can_add_product_to_basket(browser, link):
 
 @pytest.mark.skip(reason="module 4.3 step 6 -- expected fail")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 	page = ProductPage(browser, link)     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 	page.open()                           # открываем страницу
 	page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
 	page.add_product_to_basket()          # жмем кнопку добавить в корзину
 	page.should_not_be_success_message()
 def test_guest_cant_see_success_message(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 	page = ProductPage(browser, link)     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 	page.open()                           # открываем страницу
 	page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
 	page.should_not_be_success_message()
 @pytest.mark.skip(reason="module 4.3 step 6 -- expected fail")
 def test_message_disappeared_after_adding_product_to_basket(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 	page = ProductPage(browser, link)     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 	page.open()                           # открываем страницу
 	page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
@@ -59,13 +58,11 @@ def test_guest_should_see_login_link_on_product_page(browser):
 	page.should_be_login_link()
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 	page = MainPage(browser, link)
 	page.open()
 	page.should_be_login_link()
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 	page = BasketPage(browser, link)
 	page.open()
 	page.go_to_basket_now()
@@ -86,14 +83,12 @@ class TestUserAddToBasketFromProductPage():
 		login_page.register_new_user(email, password)
 		login_page.should_be_authorized_user()
 	def test_user_cant_see_success_message(self, browser):
-		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 		page = ProductPage(browser, link)     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 		page.open()                           # открываем страницу
 		page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
 		page.should_not_be_success_message()
 	@pytest.mark.need_review
 	def test_user_can_add_product_to_basket(self, browser):
-		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 		page = ProductPage(browser, link)     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
 		page.open()                           # открываем страницу
 		page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
