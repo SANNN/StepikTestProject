5fe4460ae03228ad21cdc8df4a771344e1d2df62
Anna Serova <serova@hpe.com>
2020-04-17 20:56:01 +0300

    added markers for review, removed few comments

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -7,13 +7,9 @@ class BasketPage(BasePage):
 	def go_to_basket_now(self):
 		self.openBasket(*BasketLocators.MAIN_BASKET_BUTTON)
 	def should_be_no_products_in_basket(self):
-		# assert self.browser.find_element(*BasketLocators.ITEMS_TO_BUY).text == "Items to buy now",\
-		# f"Basket is empty"
 		assert self.is_not_element_present(*BasketLocators.ITEMS_TO_BUY), (
 			"Product is presented in basket")
 	def should_be_empty_basket_message(self):
 		basket_msg = self.browser.find_element(*BasketLocators.EMPTY_BASKET_MSG).text
-		# assert basket_msg == "Your basket is empty. Continue shopping",\
-		# f"Message about empty basket is missing but found '{basket_msg}'"
 		assert self.is_element_present(*BasketLocators.EMPTY_BASKET_MSG), (
 			f"Message about empty basket is missing but found '{basket_msg}'")
--- test_product_page.py
+++ test_product_page.py
@@ -6,6 +6,7 @@ import time
 import pytest
 
 #@pytest.mark.skip(reason="module 4.3 step 4 -- expected pass")
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", \
 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1", \
 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2", \
@@ -56,11 +57,13 @@ def test_guest_should_see_login_link_on_product_page(browser):
 	page = MainPage(browser, link)
 	page.open()
 	page.should_be_login_link()
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 	page = MainPage(browser, link)
 	page.open()
 	page.should_be_login_link()
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 	page = BasketPage(browser, link)
@@ -88,6 +91,7 @@ class TestUserAddToBasketFromProductPage():
 		page.open()                           # открываем страницу
 		page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
 		page.should_not_be_success_message()
+	@pytest.mark.need_review
 	def test_user_can_add_product_to_basket(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 		page = ProductPage(browser, link)     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
